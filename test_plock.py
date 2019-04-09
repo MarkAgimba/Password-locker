ee77d357dfd2db2e384dd25859955ea89d50bb2a
Mark Agimba <markaguero16@gmail.com>
2019-04-09 12:32:41 +0300

    add TestProfile

--- test_plock.py
+++ test_plock.py
@@ -1,3 +1,10 @@
-import unittest
 import pyperclip
+import unittest
 from plock import Profile, Credential
+
+class TestProfile(unittest.TestCase):
+     '''
+    Test class that defines test cases for the profile class behaviours.
+    Args:
+        unittest.TestCase: helps in creating test cases
+    '''
