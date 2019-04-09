48ece2d5087c04d3d326ed72e65b665d87172347
Mark Agimba <markaguero16@gmail.com>
2019-04-09 12:40:54 +0300

    test to check proper profile creation

--- test_plock.py
+++ test_plock.py
@@ -14,3 +14,12 @@ class TestProfile(unittest.TestCase):
         Function to create a profile account before each test
         '''
         self.new_profile = Profile('mark', 'aguero', 'sergio10')
+
+    def test__init__(self):
+        '''
+        Test to if check the initialization/creation of profile instances is properly done
+        '''
+        self.assertEqual(self.new_profile.first_name, 'mark')
+        self.assertEqual(self.new_profile.last_name, 'aguero')
+        self.assertEqual(self.new_profile.password, 'sergio10')
+
