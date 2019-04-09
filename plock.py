354415f5ae1f337572ca3d7547fe89c9340b0f92
Mark Agimba <markaguero16@gmail.com>
2019-04-09 12:19:15 +0300

    add Credential class

--- plock.py
+++ plock.py
@@ -27,3 +27,21 @@ class Profile:
         Function to save a newly created user instance
         '''
         Profile.profiles_list.append(self)
+
+class Credential:
+   '''
+   Class to create  account credentials, generate passwords and save their information
+   '''
+   # Class Variables
+   credentials_list = []
+   profile_credentials_list = []
+   @classmethod
+   def check_profile(cls, first_name, password):
+        '''
+        Method that checks if the name and password entered match entries in the users_list
+        '''
+        current_profile = ''
+        for profile in Profile.profiles_list:
+            if (profile.first_name == first_name and profile.password == password):
+                current_profile = profile.first_name
+        return current_profile
