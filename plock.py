0338c0fc82c7ce56ad64e6e9841e996b44fe0fc4
Mark Agimba <markaguero16@gmail.com>
2019-04-09 11:57:11 +0300

    add class:profile

---
+++ plock.py
@@ -0,0 +1,14 @@
+import random
+import string
+import pyperclip
+
+# 
+global profiles_list
+
+class Profile:
+    '''
+    Class to create and save profile information
+    '''
+    # Class Variables
+    # global profiles_list
+    profiles_list = []
